536a034a09e8df428236e976e673ab4269994ca1
Raikadier <138398472+Raikadier@users.noreply.github.com>
2025-05-10 11:19:37 -0500

    Add files via upload

---
+++ BLL/IListSearchForEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    interface IListSearchForEntity <T>
+    {
+        List<T> SearchForEntity(int id);
+    }
+}
---
+++ ENTITY/Propietario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public class Propietario : Persona
+    {
+        public List<Mascota> Mascotas { get; set; } = new List<Mascota>();
+        public override string ToString()
+        {
+            return $"{Id};{Nombre};{Apellido};{Cedula};{Telefono}";
+        }
+    }
+}
